2df3ab0d7a05059dcd4f9aaf7c76728a26a347f8
Marlon <martianworm17@gmail.com>
2018-05-24 09:32:53 +0800

    Remove extra whitespace

--- com.gowpar.redis/src/main/java/com/gowpar/com/gowpar/redis/RedisJava.java
+++ com.gowpar.redis/src/main/java/com/gowpar/com/gowpar/redis/RedisJava.java
@@ -5,7 +5,7 @@ import redis.clients.jedis.Jedis;
 public class RedisJava {
 	
 	public static void main(String[] args) {
-		// Connecting  to Redis server on localhost
+		// Connecting to Redis server on localhost
 		Jedis jedis = new Jedis("localhost");
 		
 		System.out.println("Connection to server successfully");
